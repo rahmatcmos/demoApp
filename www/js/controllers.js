486522d36d5e1fce5fd5886a4c1892ee9588974e
Asrul Hanafi <hanafi.asrul@gmail.com>
2015-02-25 09:57:35 +0700

    demo app android
    First App

---
+++ www/js/controllers.js
@@ -0,0 +1,91 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+})
+
+.controller('Data', function($scope, $http) {
+  getPengguna();
+
+  $scope.refresh = function(){
+    getPengguna();
+  }
+
+  function getPengguna(){
+    $http.get("http://localhost/Tester/get.php").success(function(data){
+      $scope.pengguna = data;
+      $scope.$broadcast('scroll.refreshComplete');
+    });
+  };
+})
+
+.controller('map', function($scope, $ionicLoading){
+  google.maps.event.addDomListener(window, 'load', function() {
+    var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+
+    var mapOptions = {
+        center: myLatlng,
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    navigator.geolocation.getCurrentPosition(function(pos) {
+        map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+        var myLocation = new google.maps.Marker({
+            position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+            map: map,
+            title: "My Location"
+        });
+    });
+
+    $scope.map = map;
+  });
+})
+
+.controller('chart', function($scope){
+});
