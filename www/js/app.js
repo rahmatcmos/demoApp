039cefcd259ad3c70d47de0077d1f8417c0b0f22
Asrul Hanafi <hanafi.asrul@gmail.com>
2015-03-07 07:53:48 +0700

    ng-cordova Camera

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['angles' ,'ionic', 'starter.controllers'])
+angular.module('starter', ['angles' ,'ionic', 'ngCordova','starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -69,6 +69,17 @@ angular.module('starter', ['angles' ,'ionic', 'starter.controllers'])
       'menuContent': {
         templateUrl: "templates/chart.html",
         controller: 'chart'
+      }
+    }
+  })
+
+  //Halaman Camera
+  .state('app.camera', {
+    url: "/camera",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/camera.html",
+        controller: 'camera'
       }
     }
   })
